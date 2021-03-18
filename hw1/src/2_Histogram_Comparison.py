df32e3c8de865503da74c7996d91083d4c9d749e
NightKirie <qwer55113322@gmail.com>
2021-03-18 20:08:37 +0800

    Adjusting threshold for q2

--- hw1/src/2_Histogram_Comparison.py
+++ hw1/src/2_Histogram_Comparison.py
@@ -3,18 +3,21 @@ import numpy as np
 import cv2 
 import matplotlib.pyplot as plt
 
-def Histogram_Comparison_Gray(file_list, t=10):
+def Histogram_Comparison_Gray(file_list, t):
     img_1 = cv2.cvtColor(cv2.imread(file_list[0]), cv2.COLOR_BGR2GRAY).astype("int16")
     img_1_hist = np.bincount(img_1.reshape(img_1.shape[0] * img_1.shape[1]), minlength=256)
     shot_change_index = []
+    sd_list = []
     for i in range(1, len(file_list)-1):
         img_2 = cv2.cvtColor(cv2.imread(file_list[i]), cv2.COLOR_BGR2GRAY).astype("int16")
         img_2_hist = np.bincount(img_2.reshape(img_2.shape[0] * img_2.shape[1]), minlength=256)
-        # sd = np.sum(((img_1_hist - img_2_hist) ** 2) / img_2_hist)
+        # sd = np.sum(((img_1_hist - img_2_hist) ** 2) / img_2_hist) # bad result
         sd = np.sum(abs(img_1_hist - img_2_hist))
         img_1_hist = img_2_hist
-        shot_change_index.append(sd)
-    plt.plot(shot_change_index)
+        if sd > t:
+            shot_change_index.append(i)
+        sd_list.append(sd)
+    plt.plot(sd_list)
     plt.show()
     return shot_change_index
 
@@ -29,19 +32,19 @@ if __name__ == "__main__":
     news_file_list = []
     for dir_path, _, file_list in os.walk("../news_out"):
         news_file_list = [os.path.join(dir_path, file) for file in file_list]
-    print("Histogram comparison for shot-change detection of news.mpg: ", Histogram_Comparison_Gray(news_file_list))
+    print("Histogram comparison for shot-change detection of news.mpg: ", Histogram_Comparison_Gray(news_file_list, sqrt(352*240)*100))
     print()
 
     # # soccer
     # soccer_file_list = []
     # for dir_path, _, file_list in os.walk("../soccer_out"):
     #     soccer_file_list = [os.path.join(dir_path, file) for file in file_list]
-    # print("Histogram comparison for shot-change detection of news.mpg: ", Histogram_Comparison_Gray(soccer_file_list))
+    # print("Histogram comparison for shot-change detection of news.mpg: ", Histogram_Comparison_Gray(soccer_file_list, (360*240)*0.25))
     # print()
 
     # # ngc
     # ngc_file_list = []
     # for dir_path, _, file_list in os.walk("../ngc_out"):
     #     ngc_file_list = [os.path.join(dir_path, file) for file in file_list]
-    # print("Histogram comparison for shot-change detection of news.mpg: ", Histogram_Comparison_Gray(ngc_file_list))
+    # print("Histogram comparison for shot-change detection of news.mpg: ", Histogram_Comparison_Gray(ngc_file_list, 864*480*0.25))
     # print()
